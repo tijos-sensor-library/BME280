3941e1af932f2c915767bb11b0af84aeec2c9f0b
lemon <lemon.yang@tijos.net>
2018-01-30 18:14:56 +0800

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BME280
 BOSCH BME280 combined humidity and pressure sensor driver for TiJOS
+
+| 条目         | 说明                          |
+| ---------- | --------------------------- |
+| 驱动名称       | BME280 压力传感器                |
+| 适用         | 该驱动适用于BME280 压力传感器          |
+| 通讯方式       | I2C                         |
+| Java Class | TiBME280.java               |
+| 图片         | ![bme280](./img/bme280.png) |
+
---
+++ src/test/java/tijos/framework/sensor/bme280/TiBME280Sample.java
@@ -0,0 +1,54 @@
+package tijos.framework.sensor.bme280;
+
+import java.io.IOException;
+
+import tijos.framework.devicecenter.TiI2CMaster;
+import tijos.util.Delay;
+
+public class TiBME280Sample { 
+
+	public static void main(String[] args) {
+
+		try {
+			/*
+			 * TiI2CMaster port 0
+			 */
+			int i2cPort0 = 0;
+
+			/*
+			 * Open the I2C port 
+			 */
+			TiI2CMaster i2c0 = TiI2CMaster.open(i2cPort0);
+
+			TiBME280 bme280 = new TiBME280(i2c0);
+
+			bme280.initialize();	
+			
+			int num = 1000;
+			while (num -- > 0) {
+				try {
+
+					bme280.measure();
+
+					double temperature = bme280.getTemperature();
+					double pressure = bme280.getPressure() / 100;
+					double humidity = bme280.getHumidity();
+
+					System.out.println("pressure = " + pressure + " temperature = " + temperature + " humidity = " + humidity);
+
+					Delay.msDelay(2000);
+					
+				} catch (Exception ex) {
+
+					ex.printStackTrace();
+				}
+
+			}
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		}
+
+
+	}
+
+}
